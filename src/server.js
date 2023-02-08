ac5b58f4452aadb4744a82cf4c0ae8f43e2c5341
pintex <andre.pinto9723@gmail.com>
2023-02-08 16:35:47 -0400

    fixed server

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ let app=express();
 
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname+'./dist/DISTRIBUIDORA-WEB'));
+app.use(express.static(__dirname+'/dist/DISTRIBUIDORA-WEB'));
 
 app.get('/*', (req, res) =>
-    res.sendFile(__dirname+'dist/DISTRIBUIDORA-WEB/index.html'),
+    res.sendFile(__dirname+'/dist/DISTRIBUIDORA-WEB/index.html'),
 );
 
 // Start the app by listening on the default Heroku port
