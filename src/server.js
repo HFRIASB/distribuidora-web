91b77fd4e9526e80cb603c9e358abbc73216a07c
pintex <andre.pinto9723@gmail.com>
2023-02-08 16:19:56 -0400

    index html

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/DISTRIBUIDORA-WEB'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/src/angular-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/DISTRIBUIDORA-WEB/'}),
 );
 
 // Start the app by listening on the default Heroku port
